cd17df465f9e64958267a1db2b7c757c4b897cb8
mnlivvv <tranminhnhan271099@gmail.com>
2020-04-26 22:24:02 +0700

    todos-app

--- src/App.js
+++ src/App.js
@@ -1,59 +1,174 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import TodoItem from './components/TodoItem';
-import MyTable from './components/Table';
-import TableItem from './components/TableItem';
+import tick from './img/tick.svg';
 
-function App() {
-  let todoList = [
-    'Di da bong',
-    'Di hoc',
-    'Di choi',
-    'Di massage',
-    'Di du lich'
-  ];
-  let table = [
-    {
-      company: 'Alfreds Futterkiste',
-      contact: 'Maria Anders',
-      country: 'Germany'
-    },
-    {
-      company: 'Centro comercial Moctezuma',
-      contact: 'Francisco Chang',
-      country: 'Mexico'
-    },
-    {
-      company: 'Ernst Handel',
-      contact: 'Roland Mendel',
-      country: 'Austria'
-    },
-    {
-      company: 'Island Trading',
-      contact: 'Helen Bennett',
-      country: 'UK'
-    },
-    {
-      company: 'Laughing Bacchus Winecellars',
-      contact: 'Yoshi Tannamuri',
-      country: 'Canada'
-    },
-    {
-      company: 'Magazzini Alimentari Riuniti',
-      contact: 'Giovanni Rovelli',
-      country: 'Italy'
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      filter: 'all', // active, complete
+      currentText: '',
+      itemActive: 0,
+      itemComplete: 0,
+      todos: [],
+      filterTodos: []
     }
-  ];
 
-  return (
-    <div className="App">
-      {
-        todoList.map((item, index) => <TodoItem key={index} title={item}/>)
+    this.onKeyUp = this.onKeyUp.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.filter = this.filter.bind(this);
+    this.clearCompleted = this.clearCompleted.bind(this);
+  }
+
+  onClickFinish(item) {
+    return (event) => {
+      const isComplete = item.isComplete;
+      let currentItemComplete = 0;
+      let currentItemActive = 0;
+      if(isComplete) {
+        currentItemComplete = this.state.itemComplete - 1;
+        currentItemActive= this.state.itemActive + 1;
       }
-      <MyTable database={table}></MyTable>
+      else{
+        currentItemActive= this.state.itemActive - 1;
+        currentItemComplete = this.state.itemComplete + 1;
+      }
+      const index = this.state.todos.indexOf(item);
+      this.setState({
+        ...this.state,
+        itemComplete: currentItemComplete,
+        itemActive: currentItemActive,
+        todos: [
+          ...this.state.todos.slice(0, index),
+          {
+            ...item,
+            isComplete: !isComplete
+          },
+          ...this.state.todos.slice(index + 1)
+        ]
+      })
+    }
+  }
+
+  onClickClose(item) {
+    return (event) => {
+      const isComplete = item.isComplete;
+      const index = this.state.todos.indexOf(item);
+      if(isComplete){
+        this.setState({
+          ...this.state,
+          itemComplete: this.state.itemComplete -1,
+          todos: [
+            ...this.state.todos.slice(0, index),
+            ...this.state.todos.slice(index + 1)
+          ]
+        })
+      }
+      else{
+        this.setState({
+          ...this.state,
+          itemActive: this.state.itemActive -1,
+          todos: [
+            ...this.state.todos.slice(0, index),
+            ...this.state.todos.slice(index + 1)
+          ]
+        })
+      }
+    }
+  }
+
+  onKeyUp(event) {
+    if(event.keyCode === 13)
+    {
+      let text = event.target.value;
+      if(!text){
+        return;
+      }
+      text = text.trim();
+      if(!text){
+        return;
+      }
+      this.setState({
+        ...this.state,
+        currentText: '',
+        itemActive: this.state.itemActive + 1,
+        todos: [
+          ...this.state.todos,
+          {
+            title: text,
+            isComplete: false
+          }
+        ]
+      })
+    }
+  }
+
+  onChange(event){
+      this.setState(
+        {
+          ...this.state,
+          currentText: event.target.value
+        }
+      )
+  }
+
+  filter(state) {
+    return (event) => {
+      this.setState({
+        ...this.state,
+        filter: state
+      })  
+    }
+  }
+
+  clearCompleted() {
+    let newTodos = this.state.todos.filter((item) => {
+      if(!item.isComplete)
+        return item;
+    })
+    this.setState({
+      ...this.state,
+      itemComplete: 0,
+      todos: newTodos
+    })
+  }
+
+  render() {
+    return(
+      <div className="App">
+        <h1>Todos</h1>
+        <div className="Header">
+          <img src={tick} width={32} height={32}></img>
+          <input type="text" placeholder="Add a new item..." value={this.state.currentText} onChange={this.onChange} onKeyUp = {this.onKeyUp}></input> 
+        </div>
+        {
+          this.state.todos.filter((item) => {
+            if(this.state.filter ==='all') return item;
+            else if(this.state.filter === 'active') {
+              if(!item.isComplete) return item;
+            }
+            else {
+              if(item.isComplete) return item;
+            } 
+          }).map((item, index) => 
+          <TodoItem key={index} item={item} onClickFinish={this.onClickFinish(item)} onClickClose={this.onClickClose(item)} filter={this.state.filter}></TodoItem>
+        ) 
+        }
+        <div className="footer">
+        <div className="countItemLeft">{this.state.itemActive} item{this.state.itemActive > 1 && 's'} left</div>
+        <div className="groupBtn">
+          <button className="btn" onClick={this.filter('all')}>All</button>
+          <button className="btn" onClick={this.filter('active')}>Active</button>
+          <button className="btn" onClick={this.filter('completed')}>Completed</button>
+        </div>
+        <div className="clearCompeleted">
+          <button className="btn" onClick={this.clearCompleted}>Clear Completed[{this.state.itemComplete}]</button>
+        </div>
+      </div>  
     </div>
-  );
+    )
+  }
 }
 
 export default App;
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import './TodoItem.css';
+import checkImg from'../img/check.svg';
+import checkCompleteImg from'../img/check-complete.svg';
+import close from '../img/close.svg'
 
-function TodoItem(props) {
-    return(
-        <div className="TodoItem">
-            <p> {props.title} </p>
-        </div> 
-    );
+class TodoItem extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        let className = 'TodoItem';
+        let url = checkImg;
+        if(this.props.item.isComplete) {
+            className += ' TodoItem-complete'
+            url = checkCompleteImg;
+        }
+        return(            
+            <div className={className}>
+                <img src={url} width={32} onClick={this.props.onClickFinish}></img>
+                <p>{this.props.item.title}</p>
+                <img src={close} width={16} className="close" onClick={this.props.onClickClose}></img>
+            </div>
+        );
+    }
 }
 
 export default TodoItem;
